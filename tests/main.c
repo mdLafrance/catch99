ede2306a2f8f14ac9eaf2c120cf82bf8a869409c
mdLafrance <maxlafrance97@gmail.com>
2025-01-18 00:22:42 -0500

    Cleanup test files

--- tests/main.c
+++ tests/main.c
@@ -1,13 +1,9 @@
-#define CATCH99_CONFIG_MAIN
+#define CATCH99_MAIN
 
 #include "catch99.h"
 
-int foo() {
-    return 5;
-}
-
-// Define another test case that fails
-TEST_CASE("main - test 1  line 6") {
-  int sum = -2 + -3;
-  REQUIRE(sum == -5); // This will pass
+TEST_CASE("Test tests") {
+  CHECK(1 == 1);
+  CHECK(2 == 2);
+  REQUIRE(1 == 3);
 }
--- tests/more_tests.c
+++
@@ -1,9 +0,0 @@
-#include "catch99.h"
-
-TEST_CASE("Testing some string stuff"){REQUIRE("asdf" == "asdf")}
-
-
-TEST_CASE("More tests line 6") {
-  REQUIRE(3 == 3); // This will pass
-  REQUIRE(3 == 3); // This will pass
-}
